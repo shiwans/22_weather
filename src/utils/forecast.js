05a2aafab5733787ad4cdb554be704b44ee0903a
Shiwans <shiwans.vaishya@gmail.com>
2022-12-30 18:39:13 +0530

    added some more forecast attribute

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,15 +2,15 @@ const request = require('request')
 
 const forecast = (latitude,longitude,callback) =>{
     const url = 'http://api.weatherstack.com/current?access_key=7d049990705b2452a4495af0656a4a1e&query='+ latitude +' , '+longitude +'&units=m'
-
     request({url,json:true},(error,{body})=>{
         if (error) {
             callback('Unable to connect to weather service!',undefined)
         } else if (body.error) {
             callback('Unable to find location',undefined)
         }
-        else {       
-            callback(undefined,"It is currently " + body.current.temperature + " degress out. It feel like " + body.current.feelslike +" degrees out. In "+ body.location.name)
+        else {     
+            console.log(body)
+            callback(undefined,body.current.weather_descriptions[0] +" through out the day. It is currently " + body.current.temperature + " degress out and it feel like " + body.current.feelslike +" degrees out. In "+body.location.name +" , "+body.location.country+". "+ body.request.query)
         }
 
     })
